c8cebbb76a73b09572afe2ae13b9543ea0001d95
haroldo <haroldo.alcobacas@gmail.com>
2022-02-21 17:55:50 -0300

    Class Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,36 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+	private String titulo;
+	private String descricao;
+	private int cargahoraria;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getCargahoraria() {
+		return cargahoraria;
+	}
+	public void setCargahoraria(int cargahoraria) {
+		this.cargahoraria = cargahoraria;
+	}
+	public String toString() {
+		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargahoraria=" + cargahoraria + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
+
